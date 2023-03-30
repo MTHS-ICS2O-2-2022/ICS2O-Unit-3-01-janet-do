810be8fd05b332565bf5c2dd190fda1f9254697e
janet-do <124389477+janet-do@users.noreply.github.com>
2023-03-30 16:15:24 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This file generates the area of a trapezoid
+
+"use strict"
+
+function calculate() {
+  // input
+  const sideA = parseInt(document.getElementById("sideA").value)
+  const sideB = parseInt(document.getElementById("sideB").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // process
+  const area = ((sideA * sideB) / 2) * height
+
+  // output
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+}
